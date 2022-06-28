2c52f0ddb2f9ba25692a00f02c0e34110c0950c8
mo1ein <moeinn.com@gmail.com>
2022-06-28 04:42:44 +0430

    add media info to json

--- bot.py
+++ bot.py
@@ -29,6 +29,25 @@ async def main():
             # msg_info['last_name'] = message.from_user.last_name
             msg_info['from_id'] = f'user{message.from_user.id}'
             msg_info['text'] = message.text
+            if message.video is not None:
+                msg_info['media_type'] = 'video_file'
+                msg_info['mime_type'] = message.video.mime_type
+            elif message.audio is not None:
+                msg_info['media_type'] = 'audio_file'
+                msg_info['mime_type'] = message.audio.mime_type
+            elif message.sticker is not None:
+                msg_info['media_type'] = 'sticker'
+                # TODO: sticker emojie, width, height...
+            elif message.voice is not None:
+                msg_info['media_type'] = 'voice_message'
+                msg_info['mime_type'] = message.audio.mime_type
+                # TODO: duraction time
+            elif message.document is not None:
+                # TODO
+                pass
+            else:
+                # TODO
+                pass
             messages.append(msg_info)
 
             # TODO: find username or chat_id info instead of this
@@ -40,7 +59,7 @@ async def main():
             '''
             # TODO: media, video ...
         with open('output.json', mode='w') as f:
-            json.dump(messages, f, indent=4, sort_keys=True, default=str)
+            json.dump(messages, f, indent=4, default=str)
 
 
 app.run(main())
