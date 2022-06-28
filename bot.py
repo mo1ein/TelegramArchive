323c4efe4c7ab8780bd8f11de49c11651308aa09
mo1ein <moeinn.com@gmail.com>
2022-06-28 19:48:57 +0430

    add more media types info

--- bot.py
+++ bot.py
@@ -23,31 +23,59 @@ async def main():
             msg_info['id'] = message.id
             # TODO: type format should be str???
             msg_info['type'] = 'message'
+            # date_unixtime
             msg_info['date'] = message.date
             msg_info['from'] = message.from_user.first_name
             # TODO:
             # msg_info['last_name'] = message.from_user.last_name
             msg_info['from_id'] = f'user{message.from_user.id}'
-            msg_info['text'] = message.text
+
             if message.video is not None:
                 msg_info['media_type'] = 'video_file'
                 msg_info['mime_type'] = message.video.mime_type
+                msg_info['duraction_seconds'] = message.video.duraction
+                msg_info['width'] = message.video.width
+                msg_info['height'] = message.video.height
+                # msg_info['text'] =
             elif message.audio is not None:
+                # file
+                # thumbnail
                 msg_info['media_type'] = 'audio_file'
+                # performer
+                # title
                 msg_info['mime_type'] = message.audio.mime_type
+                msg_info['duraction_seconds'] = message.audio.duraction
+                # msg_info['text'] =
             elif message.sticker is not None:
                 msg_info['media_type'] = 'sticker'
-                # TODO: sticker emojie, width, height...
+                # thumbnail
+                msg_info['sticker_emoji'] = message.sticker.emoji
+                msg_info['width'] = message.sticker.width
+                msg_info['height'] = message.sticker.height
+                # text
             elif message.voice is not None:
                 msg_info['media_type'] = 'voice_message'
-                msg_info['mime_type'] = message.audio.mime_type
-                # TODO: duraction time
+                msg_info['mime_type'] = message.voice.mime_type
+                msg_info['duraction_seconds'] = message.voice.duraction
+                # msg_info['text']
+            elif message.video_note is not None:
+                msg_info['media_type'] = 'video_note'
+                msg_info['mime_type'] = message.video_note.mime_type
+                msg_info['duraction_seconds'] = message.video_note.duraction
+                # msg_info['text']
             elif message.document is not None:
-                # TODO
-                pass
+                msg_info['media_type'] = 'document'
+                msg_info['mime_type'] = message.document.mime_type
+                # msg_info['text']
             else:
                 # TODO
                 pass
+
+            # if message.forwarded_from is not None:
+            if message.text is not None:
+                msg_info['text'] = message.text
+            else:
+                msg_info['text'] = ""
             messages.append(msg_info)
 
             # TODO: find username or chat_id info instead of this
