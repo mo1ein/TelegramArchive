32aa202e54d73843f47479a19c18a2407c72e4f6
mo1ein <moeinn.com@gmail.com>
2022-07-13 17:46:28 +0430

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,24 @@ import os
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 
-DOWNLOAD_MEDIA = {
-    'audio': False,
-    'video': True,
-    'photo': True,
-    'sticker': False,
-    'document': False,
-    'voice_message': False,
-    'video_message': False
+# TODO: set 'files' instead of 'documents'??
+MEDIA_EXPORT = {
+    'audios': False,
+    'videos': False,
+    'photos': False,
+    'stickers': False,
+    'documents': False,
+    'voice_messages': False,
+    'video_messages': False,
+    'contacts': True
 }
+
+CHAT_EXPORT = {
+    'contacts': True,
+    'personal_chats': True,
+    'public_channels': True,
+    'public_groups': True,
+    'private_channels': True,
+    'private_groups': True,
+}
+# TODO: add const sentences for print
